2f949a8b7a9bc6da84284ce903274c2929734d72
leo791114 <leo791114@hotmail.com>
2017-04-10 18:59:50 +0800

    use jquery for google map part

--- w3school method/main.js
+++ w3school method/main.js
@@ -1,4 +1,8 @@
-// $(function () {
+$(function () {
+
+    myMap();
+});
+
 function myMap() {
     var myCenter = new google.maps.LatLng(41.878114, -87.629798);
     var mapProp = {
@@ -9,19 +13,8 @@ function myMap() {
         mapTypeId: 'roadmap'
     };
     var map = new google.maps.Map(document.getElementById('googleMap'), mapProp);
-    var marker = new google.map.Marker({ position: myCenter });
+    var marker = new google.maps.Marker({
+        position: myCenter
+    });
     marker.setMap(map);
 }
-// myMap();
-// });
-
-
-// $(function () {
-//     var myCenter = new google.maps.LatLng(41.878114, -87.629798);
-//     var mapProp = { center: myCenter, zoom: 12, scrollwheel: false, draggable: false, mapTypeId: google.maps.MapTypeId.ROADMAP };
-//     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-//     var marker = new google.maps.Marker({ position: myCenter });
-//     marker.setMap(map);
-// });
-
-
